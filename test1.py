828b40b08c47a79a2fce6c7aec496de9e2f2c349
naveensai1188 <naveensai.ayila1188@gmail.com>
2023-01-12 16:21:57 +0530

    this is my first code push in git 3

---
+++ test1.py
@@ -0,0 +1,18 @@
+x = np.linspace(2,5,30)
+print(x)
+
+y = np.linspace(3,5,30)
+print(y)
+def add(x,y):
+  return np.sin(np.sqrt(x**2 + y**2))
+
+z = add(x,y
+
+ax = plt.axes(projection='3d')
+print(ax.plot3D(x,y,z))
+z = add(x,y)
+
+ax = plt.axes(projection='3d')
+print(ax.plot3D(x,y,z))
+print(ax.set_xlabel('x'))
+print(ax.set_ylabel('y'))
